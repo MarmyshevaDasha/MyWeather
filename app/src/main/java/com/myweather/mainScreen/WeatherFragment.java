30e0af0a9a1dcd7d1d22070a08bdaa8cc3182239
MarmyshevaDasha <Marmysheva2000@yandex.ru>
2020-09-30 16:24:52 +0300

    init project

---
+++ app/src/main/java/com/myweather/mainScreen/WeatherFragment.java
@@ -0,0 +1,38 @@
+package com.myweather.mainScreen;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.myweather.R;
+
+public class WeatherFragment extends Fragment {
+
+    private WeatherViewModel mViewModel;
+
+    public static WeatherFragment newInstance() {
+        return new WeatherFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.weather_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(WeatherViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/myweather/mainScreen/WeatherViewModel.java
@@ -0,0 +1,7 @@
+package com.myweather.mainScreen;
+
+import androidx.lifecycle.ViewModel;
+
+public class WeatherViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
