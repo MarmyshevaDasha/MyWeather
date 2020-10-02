14ebbf181c7548a9f52f52eabe690e58b69eeb0d
MarmyshevaDasha <Marmysheva2000@yandex.ru>
2020-10-02 15:46:39 +0300

    made sky

--- app/src/main/java/com/myweather/mainScreen/WeatherFragment.java
+++ app/src/main/java/com/myweather/mainScreen/WeatherFragment.java
@@ -41,6 +41,6 @@ public class WeatherFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ImageView imageView = view.findViewById(R.id.imageView);
-        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(imageView);
+        Picasso.get().load("https://sun9-11.userapi.com/rQU6TU2mhpXhRNw3XEO-059-Q4BH2d0gaFpVIw/0Yyct4QOMD0.jpg").into(imageView);
     }
 }
